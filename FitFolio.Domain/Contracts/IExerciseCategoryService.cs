a15b04503f5d006a00e12dc0b4561ae69d98292c
Konstantin Khetagurov <k.hetagurov@novobi.ru>
2025-07-23 11:46:24 +0300

    ExerciseCategoryService

---
+++ FitFolio.Domain/Contracts/IExerciseCategoryService.cs
@@ -0,0 +1,13 @@
+﻿using FitFolio.Data.Models;
+
+namespace FitFolio.Domain.Contracts
+{
+    public interface IExerciseCategoryService : IDomainService
+    {
+        Task<ExerciseCategory> GetExerciseAsync(Guid id);
+        Task CreateAsync(ExerciseCategory exercise);
+        Task GetAsync(int skip, int take = 20);
+        Task UpdateAsync(ExerciseCategory exercise);
+        Task DeleteAsync(ExerciseCategory exercise);
+    }
+}
---
+++ FitFolio.Domain/ExerciseCategories/ExerciseCategoryService.cs
@@ -0,0 +1,41 @@
+﻿using FitFolio.Data.Models;
+using FitFolio.Data.Repositories.Contracts;
+using FitFolio.Domain.Contracts;
+
+namespace FitFolio.Domain.ExerciseCategories
+{
+    internal class ExerciseCategoryService : IExerciseCategoryService
+    {
+        private readonly IExerciseCategoryRepository _exerciseRepository;
+
+        public ExerciseCategoryService(IExerciseCategoryRepository exerciseRepository)
+        {
+            _exerciseRepository = exerciseRepository;
+        }
+
+        public async Task CreateAsync(ExerciseCategory exerciseCategory)
+        {
+            await _exerciseRepository.CreateAsync(exerciseCategory);
+        }
+
+        public async Task DeleteAsync(ExerciseCategory exerciseCategory)
+        {
+            await _exerciseRepository.DeleteAsync(exerciseCategory);
+        }
+
+        public async Task GetAsync(int skip, int take = 20)
+        {
+            await _exerciseRepository.GetAsync(skip, take);
+        }
+
+        public async Task<ExerciseCategory> GetExerciseAsync(Guid id)
+        {
+            return await _exerciseRepository.GetByIdAsync(id);
+        }
+
+        public async Task UpdateAsync(ExerciseCategory exerciseCategory)
+        {
+            await _exerciseRepository.UpdateAsync(exerciseCategory);
+        }
+    }
+}
